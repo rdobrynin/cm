be24478c38c4c5182a265fc305883b749510671c
Roman Dobrynin <roman.dobrynin@gmail.com>
2022-11-21 17:34:33 +0200

    change: docker configs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+  );
+
+  const documentBuilder = new DocumentBuilder()
+      .setTitle('API')
+      .setVersion('1')
+      .addBearerAuth()
+      .build();
+
+  const doc = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('documentation', app, doc, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  const config = new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('The API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs', app, document);
+
+  await app.listen(3001);
+  Logger.log(`Url for Swagger [OpenApi]: ${await app.getUrl()}/docs`);
 }
 bootstrap();
